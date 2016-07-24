efd22bbed8754fab170cb052ceca7c9331e72e1c
yangqio <649755560@qq.com>
2016-07-24 13:48:11 +0800

    主页和详情

---
+++ src/js/common.js
@@ -0,0 +1,22 @@
+// 鼠标滚动事件
+	$(window).scroll(function(){
+		if($(document).scrollTop() > 0){
+			$('.fixed-c').last().css('display','block');
+		}else{
+			$('.fixed-c').last().css('display','none');
+		}
+	});
+	$('.fixed-c').last().on('click',function(){
+		$('html,body').animate({scrollTop:0},500);
+		return false;
+	})
+
+	//扫码APP
+	$('.s-app').hover(function(){
+		$('.appInfo').show();
+		$('.appItem').stop(true).animate({right:0});
+	},function(){
+		$('.appItem').stop(true).animate({right:-310},function(){
+			$('.appInfo').hide();
+		});
+	})
